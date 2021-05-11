54168ddc87467ddec6e2109746094cd1b664fbb2
NeLO12 <y.y.ymake@gmail.com>
2021-05-11 13:10:56 +0300

    hello world

---
+++ app/src/main/java/com/example/otptest/Student.java
@@ -0,0 +1,23 @@
+package com.example.otptest;
+
+public abstract class Student {
+    protected String FirstName;
+    protected String LastName;
+    protected String ClassName;
+
+    public Student(String FirstName, String LastName, String ClassName) {
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.ClassName = ClassName;
+    }
+
+    public String getFirstName() { return FirstName; }
+    public String getLastName() { return LastName; }
+    public String getClassName() { return ClassName; }
+
+    public void setFirstName(String firstName) { FirstName = firstName; }
+    public void setLastName(String lastName) { LastName = lastName; }
+    public void setClassName(String className) { ClassName = className; }
+
+    public abstract String print();
+}
--- app/src/main/java/com/example/otptest/Winner.java
+++ app/src/main/java/com/example/otptest/Winner.java
@@ -2,27 +2,27 @@ package com.example.otptest;
 
 import android.graphics.Bitmap;
 
-public class Winner /*implements Human*/{
-    private String FirstName;
-    private String LastName;
-    private String ClassName;
+public class Winner extends Student{
+
     private String VotesNumber;
     private Bitmap Bitmap;
 
     public Winner(String FirstName, String LastName, String ClassName, String VotesNumber, Bitmap Bitmap)
     {
-        this.FirstName = FirstName;
-        this.LastName = LastName;
-        this.ClassName = ClassName;
+        super(FirstName, LastName, ClassName);
         this.VotesNumber = VotesNumber;
         this.Bitmap = Bitmap;
     }
 
     public String getVotesNumber() { return VotesNumber; }
-    public String getFirstName() { return FirstName; }
-    public String getLastName() { return LastName; }
-    public String getClassName() { return ClassName; }
     public Bitmap getBitmap() { return Bitmap; }
-    public String print() { return "FN: " + this.FirstName + ", LN: " + this.LastName + ", C: " + this.ClassName + ", VN: " + this.VotesNumber;}
+
+    public void setVotesNumber(String votesNumber) { VotesNumber = votesNumber; }
+    public void setBitmap(android.graphics.Bitmap bitmap) { Bitmap = bitmap; }
+
+    @Override
+    public String print() {
+        return "Winner{" + this.FirstName + this.LastName + ", " + this.ClassName + "}";
+    }
 
 }
